ed8778e68f68d8c27e7734f88dbe718557f2634b
Juan Arillo <juanarillo@outlook.com>
2016-10-24 22:22:30 +0200

    added Microsoft Azure Mobile Service and configured xCode for support it

---
+++ Scoops/MyDependency-Bridging-header.h
@@ -0,0 +1,10 @@
+//
+//  MyDependency-Bridging-header.h
+//  Scoops
+//
+//  Created by Juan Arillo Ruiz on 24/10/16.
+//  Copyright © 2016 Juan Arillo Ruiz. All rights reserved.
+//
+
+#import <AZSClient/AZSClient.h>
+#import <MicrosoftAzureMobile/MicrosoftAzureMobile.h>
