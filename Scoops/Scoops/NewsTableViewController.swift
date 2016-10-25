4b1b3d36fac2d7ebf09bac664d0251c0ffd16d8e
Juan Arillo <juanarillo@outlook.com>
2016-10-25 23:28:17 +0200

    Added Storyboard cell, Storyboard add button, and add, delete, and read all functions

--- Scoops/Scoops/NewsTableViewController.swift
+++ Scoops/Scoops/NewsTableViewController.swift
@@ -8,24 +8,71 @@
 
 import UIKit
 
+typealias NewsRecord = Dictionary<String, AnyObject>
+
 class NewsTableViewController: UITableViewController {
     
     var client: MSClient = MSClient(applicationURL: URL(string: "http://jarilloapp.azurewebsites.net")!)
-    var model: [String]? = []
+    var model: [Dictionary<String, AnyObject>]? = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        //addNewNews("Titulo 1", "Este es el título 1", "http://", "Juan Arillo", 27, 27)
+        
         readAllNews()
     }
 
+    @IBAction func addNewNews(_ sender: AnyObject) {
+    
+        let alert = UIAlertController(title: "Nueva Noticia", message: "Escribe la noticia", preferredStyle: .alert)
+        
+        
+        let actionOk = UIAlertAction(title: "OK", style: .default) { (alertAction) in
+            let titleField = alert.textFields![0] as UITextField
+            let textField = alert.textFields![1] as UITextField
+            let photoUrlField = alert.textFields![2] as UITextField
+            let authorField = alert.textFields![3] as UITextField
+            let latitudeField = alert.textFields![4] as UITextField
+            let longitudeField = alert.textFields![5] as UITextField
+            
+            
+            self.addNewNews(titleField.text!, textField.text!, photoUrlField.text!, authorField.text!, Double(latitudeField.text!)!, Double(longitudeField.text!)!)
+            
+        }
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(actionOk)
+        alert.addAction(actionCancel)
+        alert.addTextField { (titleField) in
+            
+            titleField.placeholder = "Introduce un Título"
+            
+        }
+        
+        alert.addTextField {(textField) in
+            textField.placeholder = "Introduce la noticia"
+        }
+        
+        alert.addTextField {(photoUrlField) in
+            photoUrlField.placeholder = "Introduce imagen"
+        }
+        
+        alert.addTextField {(authorField) in
+            authorField.placeholder = "Introduce el autor"
+        }
+        
+        alert.addTextField {(latitudeField) in
+            latitudeField.placeholder = "Introduce la latitud"
+        }
+        
+        alert.addTextField {(longitudeField) in
+            longitudeField.placeholder = "Introduce la longitud"
+        }
+        
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -44,11 +91,36 @@ class NewsTableViewController: UITableViewController {
                 return
             }
             
+            
+            self.readAllNews()
+            
             print(result)
             
         }
         
     }
+    
+    // MARK: - Delete news
+    
+    func deleteNews(_ item: NewsRecord) {
+        
+        let tableMS = client.table(withName: "News")
+        
+        tableMS.delete(item) { (result, error) in
+            
+            if let _ = error {
+                print (error)
+                return
+            }
+            
+            // refrescamos la tabla
+            
+            self.readAllNews()
+            
+        }
+        
+    }
+    
     
     // MARK: - Read news
     
@@ -56,21 +128,32 @@ class NewsTableViewController: UITableViewController {
         
         let tableMS = client.table(withName: "News")
         
-        let predicate = NSPredicate(format: "author == 'Juan Arillo'")
+        // let predicate = NSPredicate(format: "author == 'Juan Arillo'")
         
-        tableMS.read(with: predicate) { (results, error) in
-            
+        tableMS.read { (results, error) in
             if let _ = error {
                 print (error)
                 return
             }
-
-            if let items = results {
+            
+            if !((self.model?.isEmpty)!) {
                 
-                print(items)
+                self.model?.removeAll()
                 
             }
             
+            if let items = results {
+                
+                //print(items)
+                for item in items.items! {
+                    self.model?.append(item as! [String : AnyObject])
+                }
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            }
         }
         
     }
@@ -80,23 +163,34 @@ class NewsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        if (model?.isEmpty)! {
+            return 0
+        }
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        if (model?.isEmpty)! {
+            return 0
+        }
+        return (model?.count)!
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELDA", for: indexPath)
 
         // Configure the cell...
+        
+        let item = model?[indexPath.row]
+        
+        cell.textLabel?.text = item?["title"] as! String?
+        cell.detailTextLabel?.text = item?["author"] as! String?
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -106,17 +200,23 @@ class NewsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .fade)
+            let item = self.model?[indexPath.row]
+            self.deleteNews(item!)
+            self.model?.remove(at: indexPath.row)
+            tableView.endUpdates()
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
