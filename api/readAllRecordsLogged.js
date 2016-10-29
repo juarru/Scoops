4e4c8cb58638cc4c7bfe068534ada59c5ad50aef
Juan Arillo <juanarillo@outlook.com>
2016-10-30 01:17:16 +0200

    New changes in sql for testing

--- api/readAllRecordsLogged.js
+++ api/readAllRecordsLogged.js
@@ -19,7 +19,7 @@ var api = {
         var user = context.user.id;
 
         var query = {
-            sql: "Select * from News where userId = "+ user
+            sql: "Select * from News"
         };
 
         context.data.execute(query)
@@ -31,5 +31,5 @@ var api = {
 
 };
 
-api.get.access = 'anonymous';
+api.get.access = 'authenticated';
 module.exports = api;
